1cdfe6645513c3caf555ec012b82ee3788ed18c9
javierpb275 <javierpb275@gmail.com>
2021-12-04 17:21:46 +0100

    building a calculator

--- CsharpFirstProject/Program.cs
+++ CsharpFirstProject/Program.cs
@@ -10,11 +10,11 @@ namespace CsharpFirstProject
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter your name: ");
-            string name = Console.ReadLine();
-            Console.Write("Enter your age: ");
-            int age = Int32.Parse(Console.ReadLine());
-            Console.WriteLine("Hello " + name + " you are " + age);
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(num1 + num2);
             Console.ReadLine();
         }
     }
