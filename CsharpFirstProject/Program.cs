12967082448d7872e6de933e6e65c5f37ae9a20c
javierpb275 <javierpb275@gmail.com>
2021-12-04 16:57:20 +0100

    getting user input

--- CsharpFirstProject/Program.cs
+++ CsharpFirstProject/Program.cs
@@ -10,16 +10,11 @@ namespace CsharpFirstProject
     {
         static void Main(string[] args)
         {
-            int num = 6;
-            num++;
-            num--;
-            Console.WriteLine(num);
-            Console.WriteLine(Math.Abs(-num));//6
-            int sixToThePowerOf2 = (int)Math.Pow(num, 2);
-            Console.WriteLine(sixToThePowerOf2);//36
-            Console.WriteLine(Math.Sqrt(sixToThePowerOf2));//6
-            Console.WriteLine(Math.Max(sixToThePowerOf2, num));//the biggest: 36
-            Console.WriteLine(Math.Round(4.5));//4
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            int age = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Hello " + name + " you are " + age);
             Console.ReadLine();
         }
     }
