8e306cf50ad4051f6d57f7ce1611d9b2a379507e
javierpb275 <javierpb275@gmail.com>
2021-12-04 13:12:01 +0100

    printed hello world in console (readline to keep console open to see the message until press enter)

--- CsharpFirstProject/Program.cs
+++ CsharpFirstProject/Program.cs
@@ -10,6 +10,8 @@ namespace CsharpFirstProject
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
         }
     }
 }
