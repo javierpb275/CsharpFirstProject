7e4d480e3e76bc37bd78636fcc32aed0822be211
javierpb275 <javierpb275@gmail.com>
2021-12-04 13:58:04 +0100

    data types

--- CsharpFirstProject/Program.cs
+++ CsharpFirstProject/Program.cs
@@ -11,12 +11,16 @@ namespace CsharpFirstProject
         static void Main(string[] args)
         {
 
-            string characterName = "John";
-            int characterAge;
-            characterAge = 35;
+            string text = "hello";
+            char letter = 'a';
+            int integer = 1;//Use int by default for any integer-based operations that can use that type, as it will be more performant than short or long
+            float myFloat = 1.0f;
+            double myDouble = 10.5;//Use double for non-integer math where the most precise answer isn't necessary.
+            decimal myDecimal = 17;//Use decimal for non - integer math where precision is needed(e.g.money and currency).
+            bool myBoolean = false;
 
-            Console.WriteLine("Hello, I am " + characterName + ". I am " + characterAge + " years old.");
-            Console.ReadLine();
+            //Console.WriteLine("Hello, I am " + characterName + ". I am " + characterAge + " years old.");
+             Console.ReadLine();
         }
     }
 }
