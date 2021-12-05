d5afc7d66e5e8e78b12d87d49badbadce3f113be
javierpb275 <javierpb275@gmail.com>
2021-12-05 21:02:34 +0100

    created class Song

---
+++ CsharpFirstProject/Song.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpFirstProject
+{
+    internal class Song
+    {
+        public string title;
+        public string artist;
+        public int duration;
+
+        public Song(string title, string artist, int duration)
+        {
+            this.title = title;
+            this.artist = artist;
+            this.duration = duration;
+        }
+    }
+}
