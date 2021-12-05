1fefde7e0aba89193ca8367d492f2d532f4de87e
javierpb275 <javierpb275@gmail.com>
2021-12-05 12:54:57 +0100

    switch statement

--- CsharpFirstProject/Program.cs
+++ CsharpFirstProject/Program.cs
@@ -10,35 +10,42 @@ namespace CsharpFirstProject
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter a number: ");
-            double num1 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Enter Operator: ");
-            string op = Console.ReadLine();
-            Console.Write("Enter a number: ");
-            double num2 = Convert.ToDouble(Console.ReadLine());
-
-            if (op == "+")
-            {
-                Console.WriteLine(num1 + num2);
-            }
-            else if (op == "-")
-            {
-                Console.WriteLine(num1 - num2);
-            }
-            else if (op == "/")
-            {
-                Console.WriteLine(num1 / num2);
-            }
-            else if (op == "*")
-            {
-                Console.WriteLine(num1 * num2);
-            }
-            else
-            {
-                Console.WriteLine("Invalid Operator");
-            }
+            Console.WriteLine(GetDay(7));
 
             Console.ReadLine();
+        }
+
+        static string GetDay(int dayNum)
+        {
+            string dayName;
+            switch (dayNum) 
+            {
+                case 0:
+                    dayName = "Sunday";
+                    break;
+                case 1:
+                    dayName = "Monday";
+                    break;
+                case 2:
+                    dayName = "Tuesday";
+                    break;
+                case 3:
+                    dayName = "Wednesday";
+                    break;
+                case 4:
+                    dayName = "Thursday";
+                    break;
+                case 5:
+                    dayName = "Friday";
+                    break;
+                case 6:
+                    dayName = "Saturday";
+                    break;
+                default:
+                    dayName = "Invalid day number";
+                    break;
+            }
+            return dayName;
         }
 
     }
