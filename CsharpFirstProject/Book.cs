ad4651c9008a622f9af915d754a2e21b316d224f
javierpb275 <javierpb275@gmail.com>
2021-12-05 19:40:39 +0100

    constructors

--- CsharpFirstProject/Book.cs
+++ CsharpFirstProject/Book.cs
@@ -11,5 +11,15 @@ namespace CsharpFirstProject
         public string title;
         public string author;
         public int pages;
+        public Book()
+        {
+            
+        }
+        public Book(string title, string author, int pages)
+        {
+            this.title = title;
+            this.author = author;
+            this.pages = pages;
+        }
     }
 }
--- CsharpFirstProject/Program.cs
+++ CsharpFirstProject/Program.cs
@@ -11,20 +11,15 @@ namespace CsharpFirstProject
         static void Main(string[] args)
         {
 
-            Book book1 = new Book();
-
-            book1.title = "Harry Potter";
-            book1.author = "JK Rowling";
-            book1.pages = 400;
-
+            Book book1 = new Book("Harry Potter", "JK Rowling", 400);
             Book book2 = new Book();
 
-            book2.title = "Lord of the Rings";
+            book2.title = "The hobbit";
             book2.author = "Tolkien";
             book2.pages = 700;
 
-            Console.WriteLine(book1.title);
-            Console.WriteLine(book2.author);
+            Console.WriteLine(book1.author);
+            Console.WriteLine(book2.pages);
 
             Console.ReadLine();
 
