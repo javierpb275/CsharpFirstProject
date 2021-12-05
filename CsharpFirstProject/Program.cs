6acd40cb61264eb602347c8a3ce560ee220ac84b
javierpb275 <javierpb275@gmail.com>
2021-12-05 13:01:51 +0100

    while and do while loops

--- CsharpFirstProject/Program.cs
+++ CsharpFirstProject/Program.cs
@@ -10,43 +10,27 @@ namespace CsharpFirstProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(GetDay(7));
+            int index = 1;
+
+            int index2 = 6;
+
+            while (index <= 5)
+            {
+                Console.WriteLine(index);
+                index++;
+            }
+
+            do
+            {
+                Console.WriteLine(index2);
+            } while (index2 <= 5);
+
+
 
             Console.ReadLine();
         }
 
-        static string GetDay(int dayNum)
-        {
-            string dayName;
-            switch (dayNum) 
-            {
-                case 0:
-                    dayName = "Sunday";
-                    break;
-                case 1:
-                    dayName = "Monday";
-                    break;
-                case 2:
-                    dayName = "Tuesday";
-                    break;
-                case 3:
-                    dayName = "Wednesday";
-                    break;
-                case 4:
-                    dayName = "Thursday";
-                    break;
-                case 5:
-                    dayName = "Friday";
-                    break;
-                case 6:
-                    dayName = "Saturday";
-                    break;
-                default:
-                    dayName = "Invalid day number";
-                    break;
-            }
-            return dayName;
-        }
+        
 
     }
 }
