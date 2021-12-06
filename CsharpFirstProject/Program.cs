79f597373b26086f84cc89c8cb20bdc4d87c9934
javierpb275 <javierpb275@gmail.com>
2021-12-06 19:32:13 +0100

    ternary operator

--- CsharpFirstProject/Program.cs
+++ CsharpFirstProject/Program.cs
@@ -11,84 +11,16 @@ namespace CsharpFirstProject
         static void Main(string[] args)
         {
 
-             /*
-              The following code creates five random OrderIDs
-              to test the fraud detection process.  OrderIDs 
-              consist of a letter from A to E, and a three
-              digit number. Ex. A123.
-            */
+            int saleAmount = 1001;
 
-            Random random = new Random();
-            string[] orderIDs = new string[5];
-            // Loop through each blank orderID
-            for (int i = 0; i < orderIDs.Length; i++)
-            {
-                // Get a random value that equates to ASCII letters A through E
-                int prefixValue = random.Next(65, 70);
-                // Convert the random value into a char, then a string
-                string prefix = Convert.ToChar(prefixValue).ToString();
-                // Create a random number, padd with zeroes
-                string suffix = random.Next(1, 1000).ToString("000");
-                // Combine the prefix and suffix together, then assign to current OrderID
-                orderIDs[i] = prefix + suffix;
-            }
-            // Print out each orderID
-            foreach (var orderID in orderIDs)
-            {
-                Console.WriteLine(orderID);
-            }
+            int discount = saleAmount > 1000 ? 100 : 50;
 
-            //------------------------------------------
-            Random dice = new Random();
 
-            int roll1 = dice.Next(1, 7);
-            int roll2 = dice.Next(1, 7);
-            int roll3 = dice.Next(1, 7);
+            Console.WriteLine($"Discount: {discount}");
 
-            int total = roll1 + roll2 + roll3;
-            Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
-
-            if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
-            {
-                if ((roll1 == roll2) && (roll2 == roll3))
-                {
-                    Console.WriteLine("You rolled triples!  +6 bonus to total!");
-                    total += 6;
-                }
-                else
-                {
-                    Console.WriteLine("You rolled doubles!  +2 bonus to total!");
-                    total += 2;
-                }
-            }
-
-            //--------------------------------------
-
-              /*
-               This code reverses a message, counts the number of times 
-               a particular character appears, then prints the results
-               to the console window.
-             */
-
-            string originalMessage = "The quick brown fox jumps over the lazy dog.";
-
-            char[] message = originalMessage.ToCharArray();
-            Array.Reverse(message);
-
-            int letterCount = 0;
-
-            foreach (char letter in message)
-            {
-                if (letter == 'o')
-                {
-                    letterCount++;
-                }
-            }
-
-            string newMessage = new String(message);
-
-            Console.WriteLine(newMessage);
-            Console.WriteLine($"'o' appears {letterCount} times.");
+            Random coin = new Random();
+            int flip = coin.Next(0, 2);
+            Console.WriteLine((flip == 0) ? "heads" : "tails");
 
             Console.ReadLine();
 
