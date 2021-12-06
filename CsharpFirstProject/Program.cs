2780713a8f08adf7595495270d751e1090e6c190
javierpb275 <javierpb275@gmail.com>
2021-12-06 17:51:06 +0100

    if exercise

--- CsharpFirstProject/Program.cs
+++ CsharpFirstProject/Program.cs
@@ -5,49 +5,38 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace CsharpFirstProject
-/*
- Think of a namespace as the last name, surname or "family name" for a type. 
-A class contains the code that implements a type. Classes are organized into 
-namespaces to prevent naming collisions. After all, when there are thousands 
-of classes, it's possible that there might be a need to reuse a class name. 
-The namespace helps to make sure no two classes have the same full name.
-*/
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //RANDOM:
-            Random dice = new Random();
-            int roll = dice.Next(1, 7);
-            Console.WriteLine(roll);
+            Random random = new Random();
+            int daysUntilExpiration = random.Next(12);
+            int discountPercentage = 0;
 
-            /*An overloaded method is defined with multiple method signatures. 
-             * Overloaded methods provide different ways to call the method or 
-             * provide different types of data.*/
+            if (daysUntilExpiration == 0)
+            {
+                Console.WriteLine("Your subscription has expired.");
+            }
+            else if (daysUntilExpiration == 1)
+            {
+                Console.WriteLine("Your subscription expires within a day!");
+                discountPercentage = 20;
+            }
+            else if (daysUntilExpiration <= 5)
+            {
+                Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+                discountPercentage = 10;
+            }
+            else if (daysUntilExpiration <= 10)
+            {
+                Console.WriteLine("Your subscription will expire soon.  Renew now!");
+            }
 
-            int number = 7;
-            string text = "seven";
-
-            Console.WriteLine(number);
-            Console.WriteLine();
-            Console.WriteLine(text);
-
-            Random dice2 = new Random();
-            int roll1 = dice2.Next();
-            int roll2 = dice2.Next(101);
-            int roll3 = dice2.Next(50, 101);
-
-            Console.WriteLine($"First roll: {roll1}");
-            Console.WriteLine($"Second roll: {roll2}");
-            Console.WriteLine($"Third roll: {roll3}");
-
-            //get larger number:
-            int firstValue = 500;
-            int secondValue = 600;
-            int largerValue;
-            largerValue = Math.Max(firstValue, secondValue);
-            Console.WriteLine(largerValue);
+            if (discountPercentage > 0)
+            {
+                Console.WriteLine($"Renew now and save {discountPercentage}%.");
+            }
 
             Console.ReadLine();
 
