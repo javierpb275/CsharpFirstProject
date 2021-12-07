d1c7022657b6b85d3c402719795b75ebf7d73ceb
javierpb275 <javierpb275@gmail.com>
2021-12-07 13:23:49 +0100

    array methods and some string methods

--- CsharpFirstProject/Program.cs
+++ CsharpFirstProject/Program.cs
@@ -14,86 +14,149 @@ namespace CsharpFirstProject
 
             ItalianChef iChef1 = new ItalianChef();
 
-            //By adding the casting instruction (int), we're telling the C# compiler
-            //that we understand it's possible we'll lose that precision,
-            //and in this situation, it's fine.
-            decimal myDecimal = 3.14m;
-            Console.WriteLine($"decimal: {myDecimal}");
+            //sort and reverse:
+            string[] pallets = { "B14", "A11", "B12", "A13" };
 
-            int myInt = (int)myDecimal;
-            Console.WriteLine($"int: {myInt}");
+            Console.WriteLine("Sorted...");
+            Array.Sort(pallets);
+            foreach (var pallet in pallets)
+            {
+                Console.WriteLine($"-- {pallet}");
+            }
 
-            //casting a decimal into a float is a narrowing conversion because I'm losing precision.
-            decimal myDecimal2 = 1.23456789m;
-            float myFloat = (float)myDecimal;
+            Console.WriteLine("");
+            Console.WriteLine("Reversed...");
+            Array.Reverse(pallets);
+            foreach (var pallet in pallets)
+            {
+                Console.WriteLine($"-- {pallet}");
+            }
 
-            Console.WriteLine($"Decimal: {myDecimal2}");
-            Console.WriteLine($"Float: {myFloat}");
+            //clear:
+            string[] pallets2 = { "B14", "A11", "B12", "A13" };
+            Console.WriteLine("");
 
-            //tostring method:
-            int first = 5;
-            int second = 7;
-            string message = first.ToString() + second.ToString();
-            Console.WriteLine(message);
+            Array.Clear(pallets2, 0, 2);
+            Console.WriteLine($"Clearing 2 ... count: {pallets2.Length}");
+            foreach (var pallet in pallets2)
+            {
+                Console.WriteLine($"-- {pallet}");
+            }
+            /*When you run the code, you'll see that the values stored in the first two elements of the array have been cleared out. 
+             * As we can see from the Length property and the foreach statement, the elements still exist, but they're now empty.*/
 
-            //parse method:
-            string first2 = "5";
-            string second2 = "7";
-            int sum = int.Parse(first2) + int.Parse(second2);
-            Console.WriteLine(sum);
+            //access value of cleared element:
+            string[] pallets3 = { "B14", "A11", "B12", "A13" };
+            Console.WriteLine("");
 
-            //Convert class:
-            string value1 = "5";
-            string value2 = "7";
-            int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
+            Console.WriteLine($"Before: {pallets3[0]}");
+            Array.Clear(pallets3, 0, 2);
+            Console.WriteLine($"After: {pallets3[0]}");
+
+            Console.WriteLine($"Clearing 2 ... count: {pallets3.Length}");
+            foreach (var pallet in pallets3)
+            {
+                Console.WriteLine($"-- {pallet}");
+            }
+
+            /*If you focus on the line of output After: , you may think that the value stored in pallets[0] is an empty string. 
+             * However, the C# Compiler implicitly converts the null value to an empty string for presentation.*/
+
+            //resize:
+            string[] pallets4 = { "B14", "A11", "B12", "A13" };
+            Console.WriteLine("");
+
+            Array.Clear(pallets4, 0, 2);
+            Console.WriteLine($"Clearing 2 ... count: {pallets4.Length}");
+            foreach (var pallet in pallets4)
+            {
+                Console.WriteLine($"-- {pallet}");
+            }
+
+            Console.WriteLine("");
+            Array.Resize(ref pallets4, 6);
+            Console.WriteLine($"Resizing 6 ... count: {pallets4.Length}");
+
+            pallets4[4] = "C01";
+            pallets4[5] = "C02";
+
+            foreach (var pallet in pallets4)
+            {
+                Console.WriteLine($"-- {pallet}");
+            }
+
+            Console.WriteLine("");
+            Array.Resize(ref pallets4, 3);
+            Console.WriteLine($"Resizing 3 ... count: {pallets4.Length}");
+
+            foreach (var pallet in pallets4)
+            {
+                Console.WriteLine($"-- {pallet}");
+            }
+            /*Notice that calling Array.Resize() didn't eliminate the first two null elements. 
+             * Rather, it removed the last three elements -- despite the fact that they contained string values.*/
+
+            //STRINGS:
+
+            //ToCharArray
+            string value = "abc123";
+            char[] valueArray = value.ToCharArray();
+            Array.Reverse(valueArray);
+            string result = new string(valueArray);
             Console.WriteLine(result);
 
-            //The Convert class is best for converting fractional numbers
-            //into whole numbers (int) because it rounds up the way you would expect.
+            //Join
+            string value2 = "abc123";
+            char[] valueArray2 = value2.ToCharArray();
+            Array.Reverse(valueArray2);
+            // string result2 = new string(valueArray2);
+            string result2 = String.Join(",", valueArray2);
+            Console.WriteLine(result2);//3,2,1,c,b,a
+
+            //Split
+            string value3 = "abc123";
+            char[] valueArray3 = value3.ToCharArray();
+            Array.Reverse(valueArray3);
+            // string result3 = new string(valueArray3);
+            string result3 = String.Join(",", valueArray3);
+            Console.WriteLine(result3);
+
+            string[] items = result3.Split(',');
+            foreach (string item in items)
+            {
+                Console.WriteLine(item);
+            }
+
+            //esercise1:
+            string pangram1 = "The quick brown fox jumps over the lazy dog";
+
+            string[] message1 = pangram1.Split(' ');
+            string[] newMessage1 = new string[message1.Length];
+
+            for (int i = 0; i < message1.Length; i++)
+            {
+                char[] letters1 = message1[i].ToCharArray();
+                Array.Reverse(letters1);
+                newMessage1[i] = new string(letters1);
+            }
+
+            string result1 = String.Join(" ", newMessage1);
+            Console.WriteLine(result1);//ehT kciuq nworb xof spmuj revo eht yzal god
 
 
-            int value3 = (int)1.5m; // casting truncates
-            Console.WriteLine(value3);//1 (always 1)
+            //exercise2:
+            string orderStream1 = "B123,C234,A345,C15,B177,G3003,C235,B179";
+            string[] items2 = orderStream1.Split(',');
 
-            int value4 = Convert.ToInt32(1.5m); // converting rounds up
-            Console.WriteLine(value4);//2 (if 1.499 = 1)
+            foreach (var item in items2)
+            {
+                if (item.StartsWith("B"))
+                {
+                    Console.WriteLine(item);
+                }
+            }
 
             Console.ReadLine();
-
-            //TryParse:
-
-            string value5 = "102";
-            int result2;
-            if (int.TryParse(value5, out result2))
-            {
-                Console.WriteLine($"Measurement: {result2}");
-            }
-            else
-            {
-                Console.WriteLine("Unable to report the measurement.");
-            }
-
-            //exercise:
-            string[] values = { "12.3", "45", "ABC", "11", "DEF" };
-
-            decimal total = 0m;
-            string message2 = "";
-
-            foreach (var value in values)
-            {
-                decimal number;
-                if (decimal.TryParse(value, out number))
-                {
-                    total += number;
-                }
-                else
-                {
-                    message2 += value;
-                }
-            }
-
-            Console.WriteLine($"Message: {message2}");
-            Console.WriteLine($"Total: {total}");
 
         }
 
