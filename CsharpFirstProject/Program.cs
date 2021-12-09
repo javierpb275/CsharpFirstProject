a8e93e605ad684beca9aad05a54c5815bea4f3e9
javierpb275 <javierpb275@gmail.com>
2021-12-09 17:32:32 +0100

    struct

--- CsharpFirstProject/Program.cs
+++ CsharpFirstProject/Program.cs
@@ -7,10 +7,18 @@ using CsharpFirstProject.Access;
 
 namespace CsharpFirstProject
 {
+    struct Person
+    {
+        public string Name { get; set; }
+        public string eyeColor { get; set; }
+        public int age { get; set; }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Person Person1 = new Person();
+            Person1.Name = "Pepe";
             
             Console.ReadLine();
 
