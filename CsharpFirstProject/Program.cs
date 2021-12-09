a0f4e1934f37e4be96fb8e9d0bff8c3a63e7d3e9
javierpb275 <javierpb275@gmail.com>
2021-12-09 17:47:09 +0100

    enum

--- CsharpFirstProject/Program.cs
+++ CsharpFirstProject/Program.cs
@@ -7,19 +7,34 @@ using CsharpFirstProject.Access;
 
 namespace CsharpFirstProject
 {
-    struct Person
+    public enum ProductCodes
     {
-        public string Name { get; set; }
-        public string eyeColor { get; set; }
-        public int age { get; set; }
+        Milk = 0, 
+        Juice = 1, 
+        Tea = 2
     }
+    
     internal class Program
     {
         static void Main(string[] args)
         {
-            Person Person1 = new Person();
-            Person1.Name = "Pepe";
+
+            ProductCodes test = ProductCodes.Milk;
+            Console.WriteLine(test);//Milk
+            Console.WriteLine((int)test);//0
+
+            int test2 = 1;
+            Console.WriteLine((ProductCodes)test2);//Juice
+
             
+            Console.WriteLine(test.ToString());//Milk
+
+            string test3 = "Tea";
+            ProductCodes getParse;
+            bool checkParse = Enum.TryParse(test3, out getParse);
+
+            Console.WriteLine(getParse);//Tea
+
             Console.ReadLine();
 
         }
